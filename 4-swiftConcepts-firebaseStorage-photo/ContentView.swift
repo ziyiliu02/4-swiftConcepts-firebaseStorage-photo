8bf5db19f5e71550286aff7e8ca484788344d95f
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-12 15:12:42 +0800

    Initial Commit

---
+++ 4-swiftConcepts-firebaseStorage-photo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  4-swiftConcepts-firebaseStorage-photo
+//
+//  Created by Liu Ziyi on 12/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 4-swiftConcepts-firebaseStorage-photo/__swiftConcepts_firebaseStorage_photoApp.swift
@@ -0,0 +1,17 @@
+//
+//  __swiftConcepts_firebaseStorage_photoApp.swift
+//  4-swiftConcepts-firebaseStorage-photo
+//
+//  Created by Liu Ziyi on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct __swiftConcepts_firebaseStorage_photoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
