bbb1dc4fb464d6b3135cd106a4ce645ea19503fd
Liu Ziyi <ziyi.liu02@gmail.com>
2023-08-15 10:21:46 +0800

    - Retrieve references from the db - Retrieve the photos from storage

--- 4-swiftConcepts-firebaseStorage-photo/ContentView.swift
+++ 4-swiftConcepts-firebaseStorage-photo/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     
     @State var isPickerShowing = false
     @State var selectedImage: UIImage?
+    @State var retrievedImages = [UIImage]()
     
     var body: some View {
         
@@ -41,10 +42,26 @@ struct ContentView: View {
                 }
             }
             
+            Divider()
+            
+            HStack {
+                
+                // Loop through the images and display them
+                ForEach(retrievedImages, id: \.self) { image in
+                    Image(uiImage: image)
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                }
+                
+            }
+            
         }
         .sheet(isPresented: $isPickerShowing, onDismiss: nil) {
             // Image picker
             ImagePicker(selectedImage: $selectedImage, isPickerShowing: $isPickerShowing)
+        }
+        .onAppear {
+            retrievePhotos()
         }
         
     }
@@ -79,13 +96,73 @@ struct ContentView: View {
                 
                 // Save a reference to the file in Firestore DB
                 let db = Firestore.firestore()
-                db.collection("images").document().setData(["url":path])
+//                db.collection("images").document().setData(["url":path])
+                db.collection("images").document().setData(["url":path]) { error in
+                    
+                    // If there are no errors, display the new image
+                    if error == nil {
+                        DispatchQueue.main.async {
+                            
+                            // Add the uploaded image to the list of images for display
+                            self.retrievedImages.append(self.selectedImage!)
+                        }
+                    }
+                }
             }
         }
         
-        // Save a reference to the file in Firestore DB
         
+    }
+    
+    func retrievePhotos() {
         
+        // Get the data from the database
+        let db = Firestore.firestore()
+        
+        db.collection("images").getDocuments { snapshot, error in
+            
+            if error == nil && snapshot != nil {
+                
+                var paths = [String]()
+                
+                // Loop through all the returned docs
+                for doc in snapshot!.documents {
+                    
+                    // Extract the file path and add to array
+                    paths.append(doc["url"] as! String)
+                }
+                
+                // Loop through each file path and fetch the data from storage
+                for path in paths {
+                    
+                    // Get a reference to storage
+                    let storageRef = Storage.storage().reference()
+                    
+                    // Specify the path
+                    let fileRef = storageRef.child(path)
+                    
+                    // Retrieve the data
+                    fileRef.getData(maxSize: 5 * 1024 * 1024) { data, error in
+                        
+                        // Check for errors
+                        if error == nil && data != nil {
+                            
+                            // Create a UIImage and put it into our array for display
+                            if let image = UIImage(data: data!) {
+                                DispatchQueue.main.async {
+                                    retrievedImages.append(image)
+                                }
+                            }
+
+                        }
+                    }
+                } // end loop through paths
+                
+            }
+            
+        }
+        
+        // Display the images
     }
     
 }
